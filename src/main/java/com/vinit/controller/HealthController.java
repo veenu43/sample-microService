8ba260b1b41fe62481f67a4dc34f0da692432977
vinit <vinitk0408@gmail.com>
2021-09-15 09:28:56 +0800

    commit

---
+++ src/main/java/com/vinit/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.vinit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "Success";
+    }
+
+
+}
